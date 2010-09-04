9e30e99f4ce28548073b640024f61c4378b2feae
root <root@spilka.math.unr.edu>
2010-09-03 17:12:43 -0700

    Update

--- all-update.py
+++ all-update.py
@@ -4,12 +4,14 @@ import cgitb
 cgitb.enable()
 
 import subprocess
+import time
 
 print "Content-type: text/plain"
 print
 
-cmd = 'sudo /bin/su git -c "/bin/sh update-hermes-repo.sh"'
-print "Running update script."
+cmd = 'sudo /bin/su git -c "/bin/sh update-repos.sh"'
+print "Time: ", time.time()
+print "Running update all repos script."
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
 	stderr=subprocess.STDOUT)
 output = p.stdout.read()
--- update-repos.sh
+++ update-repos.sh
@@ -3,17 +3,13 @@
 set +e 
 echo "Running as:"
 id
-echo "Updating repository..."
+echo "Updating repositories..."
 cd /home/git/repositories/hermes.git
 git fetch origin master:master
+cd /home/git/repositories/femhub.git
+git fetch origin master:master
+cd /home/git/repositories/libfemhub.git
+git fetch origin master:master
+cd /home/git/repositories/agros2d.git
+git fetch origin master:master
 echo "  Done."
-echo "Updating docs..."
-cd /var/www2/hermes/hermes
-export GIT_DIR=`pwd`/.git
-/usr/bin/git reset --hard
-/usr/bin/git clean -dfx
-/usr/bin/git pull origin master
-echo "Building doc ..."
-cd doc
-make html
-echo "Done Building doc !"
